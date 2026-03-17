74f4823c3e5490a5a1356a6ccec7274979208404
Nauman Shiraz <nauman_shiraz@hotmail.com>
2026-03-17 22:16:25 +0500

    fixed lint issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
     fetchTasks();
   };
 
-  let unusedvar = 'unused variable here';
+  //let unusedvar = 'unused variable here';
 
   return (
     <div className="min-h-screen bg-gray-50">
